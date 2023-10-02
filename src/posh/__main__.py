38480ff261b740f441b6c2d750b27dccac0f392f
Eris Fletcher <fletcher.eris@gmail.com>
2023-10-02 17:31:00 -0600

    default path is now the cwd & added version option

--- src/posh/__main__.py
+++ src/posh/__main__.py
@@ -1,30 +1,33 @@
 from argparse import ArgumentParser
-from os import truncate
 from pathlib import Path
 
 from loguru import logger
 
-from posh.interpreter.interpreter import Interpreter
-
 from . import __version__
 from .colours import FgColour, add_colours
+from .commands.file_system.path_utils import parse_path
 from .interpreter import Console, FileIntepreter
+from .interpreter.interpreter import Interpreter
 
 
 @logger.catch
 def main() -> None:
-    try:
-        truncate(Path(__file__).parent / "data" / "posh.log", 0)
-    except FileNotFoundError:
-        pass
     logger.remove()
     logger.add(
         Path(__file__).parent / "data" / "posh.log",
         delay=True,
         enqueue=True,
+        level="WARNING",
     )
 
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog="posh")
+    parser.add_mutually_exclusive_group().add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        default=False,
+        help="print the interpreter version and exit",
+    )
     parser.add_mutually_exclusive_group().add_argument(
         "file_path", nargs="?", default=None, type=str, help="file to interpret"
     )
@@ -32,16 +35,20 @@ def main() -> None:
         "-d",
         "--starting_directory",
         type=str,
-        default="",
+        default=Path.cwd(),
         help="starting directory",
     )
 
     arguments = parser.parse_args()
 
-    if not arguments.starting_directory:
-        starting_directory = Path.home()
-    else:
-        starting_directory = Path(arguments.starting_directory).expanduser().resolve()
+    if arguments.version:
+        print(__version__)
+        return
+
+    starting_directory: str | Path = arguments.starting_directory
+    if not isinstance(starting_directory, Path):
+        starting_directory = parse_path(starting_directory, Path.cwd())
+
         if not starting_directory.exists():
             print(
                 add_colours(
@@ -53,12 +60,7 @@ def main() -> None:
 
     interpreter: Interpreter
     if arguments.file_path is not None:
-        file_path = Path(arguments.file_path).expanduser()
-
-        if not file_path.is_absolute():
-            file_path = starting_directory / file_path
-        else:
-            file_path = file_path.resolve()
+        file_path = parse_path(arguments.file_path, Path.cwd())
 
         if not file_path.is_file():
             print(
@@ -69,7 +71,7 @@ def main() -> None:
             )
             return
 
-        interpreter = FileIntepreter(Path(starting_directory), file_path)
+        interpreter = FileIntepreter(starting_directory, file_path)
     else:
         print(
             f"posh {__version__}  Copyright (C) 2023  Eris\n"
@@ -77,7 +79,7 @@ def main() -> None:
             "This is free software, and you are welcome to redistribute it\n"
             "under certain conditions; for details type 'license -p'.\n"
         )
-        interpreter = Console(Path(starting_directory))
+        interpreter = Console(starting_directory)
 
     interpreter.main()
 
